b8a6cc203c8000c84e50532d37fbf4bd257a8e4f
Alain-Kay <kazadikayumba@gmail.com>
2023-08-01 20:56:33 +0200

    route creation Post

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::post('/posts/create', [PostController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
